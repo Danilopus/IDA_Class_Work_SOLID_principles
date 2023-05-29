9c54d384f595605168e5bd53afab931915581a8c
AlexandrZolotukhin <zoloto.alex08@yandex.ru>
2023-05-29 21:22:17 +0300

    Add project files.

---
+++ IDA_Class_Work_SOLID_principles/IDA_Class_Work_DEFAULT.cpp
@@ -0,0 +1,398 @@
+﻿#include <iostream>
+#include <string>
+#include <windows.h>
+#include <time.h>
+#include <algorithm>
+//#include <array>
+
+// Service functions prototypes ------------------------------------
+int Get_Int_SA();			// String-analysis вариант фильтра INTEGER
+int Get_Int_Positive();		// String-Analysis вариант проверки ввода INTEGER_POSITIVE		
+int Get_Int_PG();			// peek&get вариант проверки ввода INTEGER	
+double Get_Dbl_SA();		// String-analysis вариант фильтра DOUBLE.
+double Get_Dbl_Positive();	// String-analysis вариант фильтра DOUBLE_POSITIVE
+double Get_Dbl_PG();		// peek&get вариант проверки ввода DOUBLE	
+int Get_Random(int Range_min, int Range_max);
+double Get_Random(double Range_min, double Range_max);
+template <typename ANY> void Fill_Array_with_Random(ANY arr[], int Size, ANY Range_min, ANY Range_max);
+template <typename ANY> void Array_Console_Out(ANY arr[], int Size);
+
+// Class Work DD.MM.YYYY -------------------------------------------	
+
+//SOLID principles:
+
+// S - single responsibility principle	// Класс выполняет одну функцию/действие
+// O - open/close principle				// принцип открыто для расширений, закрыто для модификаций
+// L - 
+
+
+
+
+
+
+// O - open/close principle				// принцип открыто для расширений, закрыто для модификаций
+class Animal
+{
+private:
+	std::string _type;
+public:
+	Animal(std::string type) : _type(type)
+	{
+
+	}
+	std::string GetType() const
+	{
+		return _type;
+	}
+	//virtual - ключевое слово для прототипа функции для наследования
+	virtual std::string GetSound() = 0;
+};
+
+void AnimalSound_non_SOLID(Animal& animal_1)
+{
+	if (animal_1.GetType() == "Lion")
+		std::cout << "RRRRRR";
+	else if (animal_1.GetType() == "Cat")
+		std::cout << "Meeow";
+	else if (animal_1.GetType() == "Dog")
+		std::cout << "Fav";
+}
+
+class Cow : public Animal
+{
+public:
+	Cow() : Animal("Cat")
+	{
+
+	}
+	//std::string GetSound()
+	std::string GetSound() override
+	{
+		return "Gavgav";
+	}
+};
+
+class Parrot : public Animal
+{
+public:
+	Parrot() : Animal("Parrot")
+	{
+
+	}
+	std::string GetSound()
+	//std::string GetSound() override
+	{
+		return "Caramba";
+	}
+};
+
+void AnimalSound_SOLID_based(Animal& animal_1)
+{
+	//animal_1.GetSound();
+	std::cout << animal_1.GetSound() <<"\n\n";
+}
+
+
+//Task 1
+void Task_1()
+{
+	Cow* cow = new Cow;
+	Parrot* parrot = new Parrot;
+	AnimalSound_SOLID_based(*cow);
+	AnimalSound_SOLID_based(*parrot);
+}
+
+
+class my_own_vector
+{
+private:
+	int _size;
+	int* _index;
+public:
+	my_own_vector()
+	{
+		_size = 0;
+		_index = nullptr;
+	}
+
+	void push_back(int element)
+	{
+		_size++;
+		int* new_index = new int[_size];
+		if (_index == nullptr)
+		{
+			new_index[0] = element;
+			_index = new_index;
+			return;
+		}
+
+		for (int i = 0; i < _size - 1; i++)
+		{
+			new_index[i] = _index[i];
+		}
+
+		new_index[_size - 1] = element;
+
+		delete[] _index;
+		_index = new_index;
+
+	}
+
+	void array_resize()
+	{
+
+	}
+
+	void ShowElements()
+	{
+		for (int i = 0; i < _size; i++)
+		{
+			std::cout << i << " element: " << _index[i] << '\n';
+		}
+	}
+};
+
+//Task 2
+void Task_2()
+{
+	my_own_vector vector_1;
+	vector_1.push_back(5);
+	vector_1.push_back(10);
+	vector_1.push_back(15);
+	vector_1.push_back(25);
+	vector_1.push_back(50);
+	vector_1.ShowElements();
+}
+
+
+// MAIN ------- MAIN ------- MAIN ------- MAIN ------- MAIN --------
+int main()
+{
+srand(time(NULL));
+		
+Task_1();
+Task_2();
+
+
+
+
+
+std::cout << "\n\n";
+system("pause");	
+}
+
+// Service functions -----------------------------------------
+int Get_Int_SA() {
+	std::string a;
+	std::cin.sync(); 
+	std::cin.clear(); 
+	getline(std::cin, a);
+	if (a != "")
+	{
+
+		if ((a.find_first_not_of("-0123456789") == -1))
+		{
+			for (int i = 1; i < a.size(); i++) // определяем есть ли минусы кроме первого символа
+			{
+				if (a[i] == '-') 
+				{
+					std::cout << "Input Error: Minus misstanding. [INTEGER] expected.\n";
+					return Get_Int_SA();
+				}
+			}
+			try
+			{
+				return stoi(a);
+			}
+			catch (std::out_of_range)
+			{
+				std::cout << "Input Error: Data type overflow. [INTEGER] expected.\n";
+				return Get_Int_SA();
+			}
+
+
+		}
+		else
+		{
+			std::cout << "Input Error: Incorrect symbol. [INTEGER] expected.\n";
+			return Get_Int_SA();
+		}
+	}
+	else
+	{
+		std::cout << "Input Error: NULL input. [INTEGER] expected.\n";
+		return Get_Int_SA();
+	}
+}
+int Get_Int_Positive()
+{
+	std::string a;
+	std::cin.sync();
+	std::cin.clear();
+	getline(std::cin, a);
+	if (a != "") 
+	{
+
+		if ((a.find_first_not_of("0123456789") == -1))
+		{
+			try{
+				return stoi(a);
+			}
+			catch (std::out_of_range){
+				std::cout << "Input Error: overflow. Positive [INTEGER] expected.\n";
+			}
+		}
+		else
+		{
+			std::cout << "Input Error: Positive [INTEGER] expected.\n";
+			return Get_Int_Positive();
+		}		
+	}
+	else
+		std::cout << "Input Error: NULL input. Positive [INTEGER] expected.\n";
+	return Get_Int_Positive();
+}
+int Get_Int_PG()
+{
+	int a;
+	while (!(std::cin >> a) || (std::cin.peek() != '\n'))
+	{
+		std::cin.clear();
+		while (std::cin.get() != '\n');
+		std::cout << "Input error! [INTEGER] expected. Repeat please...\n";
+	}
+	return a;
+}
+double Get_Dbl_SA(){
+	std::string a;
+	std::cin.sync();
+	getline(std::cin, a);
+	if (a == "") { std::cout << "\nInput Error : NULL input\n"; return Get_Dbl_SA(); }
+	if ((a.find_first_not_of("-.0123456789") == -1))
+	{
+		for (int i = 1; i < a.size(); i++) // Проверяем отсутсвие лишних минусов
+		{
+			if (a[i] == '-')
+			{
+				std::cout << "\nInput Error: Minus misstanding. [DOUBLE] expected.\n";
+				return Get_Dbl_SA();
+			}
+
+		}
+		bool Dot_Flag = 0;
+		for (int i = 0; i < a.size(); i++) // проверяем отсутсвие лишних точек
+		{
+			if (a[i] == '.')
+			{
+				if (Dot_Flag == 1)
+				{
+					std::cout << "\nInput Error: Dot misstandig. [DOUBLE] expected.\n";
+					return Get_Dbl_SA();
+				}
+				Dot_Flag = 1;
+			}
+
+		}		 	
+				try{
+					return stod(a);
+				}
+				catch (std::out_of_range) {
+					std::cout << "Input Error: overflow. [DOUBLE] expected.\n";
+					return Get_Dbl_SA();					
+				}						
+	}
+	else {		
+		std::cout << "Incorrect symbols: [DOUBLE] expected.\nUse dot instead of coma. Example 50.64\n";
+		return Get_Dbl_SA();
+	}
+}
+double Get_Dbl_Positive(){
+	std::string a, dot = ".";
+	std::cin.sync();
+	std::cin.clear();
+	getline(std::cin, a);
+	if (a != "")
+	{
+		if ((a.find_first_not_of(".0123456789") == -1))
+		{
+			bool One_Dot_already = false;
+			for (int i = 0; i < a.size(); i++)
+			{				
+				if (a[i] == dot[0] && One_Dot_already == true) //проверяем количество разделителей
+				{
+					std::cout << "Input Error: Dot misstandig. Positive [DOUBLE] expected.\n";
+					return Get_Dbl_Positive();
+				}
+				if (a[i] == dot[0])	One_Dot_already = true;
+			}
+			try
+			{				
+				return stod(a);
+			}
+			catch (std::out_of_range)
+			{
+				std::cout << "Input Error: Data type overflow. Positive [DOUBLE] expected.\n";
+				return Get_Dbl_Positive();
+			}
+		}
+		else
+		{
+			//std::cout << "Incorrect symbols: positive REAL expected.\nUse coma instead of dot. Example 50,64\n"; // да точно говорю вот даже сообщение такое было, а теперь наоборот wtf*2?!
+			std::cout << "Incorrect symbols: Positive [DOUBLE] expected.\nUse dot instead of coma. Example 50.64\n";
+			return Get_Dbl_Positive();
+		}
+	}
+	else
+	{
+		std::cout << "Input Error: NULL input. Positive [DOUBLE] expected.\n";
+		return Get_Dbl_Positive();
+	}
+	return Get_Dbl_Positive();
+}
+double Get_Dbl_PG()
+{
+	double a;
+	while (!(std::cin >> a) || (std::cin.peek() != '\n'))
+	{
+		std::cin.clear();
+		while (std::cin.get() != '\n');
+		std::cout << "Input error! [DOUBLE] expected. Repeat please...\n";
+	}
+	return a;
+}
+int Get_Random(int Range_min=-999, int Range_max=999)
+{
+	if (Range_min > Range_max) 
+	{ 
+		std::cout << "\nError: Range_min > Range_max. New range: [Range_max..Range_min]\n";
+		std::swap(Range_min, Range_max);
+	}
+	//return (Range_min + rand() % (Range_max - Range_min + 1));
+	double K_random = double((rand() % RAND_MAX)) / RAND_MAX;
+	int random = Range_min + ((Range_max - Range_min) * K_random);
+	//return Range_min + (Range_max - Range_min) * (rand() % RAND_MAX) / RAND_MAX;
+	return random;
+}
+double Get_Random(double Range_min=-9999.0, double Range_max=9999.0)
+{
+	if (Range_min > Range_max)
+	{
+		std::cout << "\nError: Range_min > Range_max. New range: [Range_max..Range_min]\n";
+		std::swap(Range_min, Range_max);
+	}
+	return 	Range_min + (Range_max - Range_min) * double(rand() % RAND_MAX) / RAND_MAX;	
+}
+template <typename ANY> void Array_Console_Out(ANY arr[], int Size)
+{
+	std::cout << "\n[";
+	for (int i = 0; i < Size; i++)
+		std::cout << arr[i] << ", ";
+	std::cout << "\b\b]\n";
+}
+template <typename ANY> void Fill_Array_with_Random(ANY arr[], int Size, ANY Range_min, ANY Range_max)
+{
+	for (int i = 0; i < Size; i++)
+	{
+		arr[i] = Get_Random(Range_min, Range_max);		
+	}
+}
